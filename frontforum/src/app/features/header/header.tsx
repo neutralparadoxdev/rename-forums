c04a36ef84d5ce8289c4188658e632d564a5213b
paradox <paradoxtwitchstreamer@gmail.com>
2023-10-27 21:55:56 -0400

    Added NewPost feature and placed it in desired locations. Updated headerto expose the Login / Signup State

--- frontforum/src/app/features/header/header.tsx
+++ frontforum/src/app/features/header/header.tsx
@@ -6,14 +6,14 @@ import { useRouter } from "next/navigation";
 export type HeaderProps = {
     title: string,
     link: string | null,
+    loginSignUpState: SignUpLoginModalPurpose | null, 
+    setLoginSignUpState: (purpose: SignUpLoginModalPurpose | null) => void
 }
 
-export const Header: FC<HeaderProps> = ({title, link} : HeaderProps) => {
+export const Header: FC<HeaderProps> = ({title, link, loginSignUpState, setLoginSignUpState} : HeaderProps) => {
     const router = useRouter();
 
     const sessionToken = localStorage.getItem('session-token')
-
-    const [loginSignupPrompt, setLoginSignupPrompt] = useState<SignUpLoginModalPurpose | null>();
 
     function logout() {
 
@@ -34,12 +34,12 @@ export const Header: FC<HeaderProps> = ({title, link} : HeaderProps) => {
     }
 
     return (<>
-        { loginSignupPrompt != null ? 
+        { loginSignUpState != null ? 
         <SignUpLoginModal 
-            purpose={loginSignupPrompt} 
-            close={() => {setLoginSignupPrompt(null);}} 
-            changePurpose={(purpose) => setLoginSignupPrompt(purpose)}
-            setAuthToken={(token) => { setLoginSignupPrompt(null); }}
+            purpose={loginSignUpState} 
+            close={() => {setLoginSignUpState(null);}} 
+            changePurpose={(purpose) => setLoginSignUpState(purpose)}
+            setAuthToken={(token) => { setLoginSignUpState(null); }}
             /> : 
             <></> }
     <header className="border-b-4 border-[blue]  flex justify-between pr-2 pl-2">
@@ -49,8 +49,8 @@ export const Header: FC<HeaderProps> = ({title, link} : HeaderProps) => {
         <div className="mt-auto mb-0">
             { sessionToken === null || sessionToken === "" ?
             <>
-            <button className="mr-2 hover:text-red-400" onClick={() => setLoginSignupPrompt(SignUpLoginModalPurpose.SignUp)}>Sign Up</button>
-            <button className="hover:text-red-400" onClick={() => setLoginSignupPrompt(SignUpLoginModalPurpose.Login)}>Log In</button>
+            <button className="mr-2 hover:text-red-400" onClick={() => setLoginSignUpState(SignUpLoginModalPurpose.SignUp)}>Sign Up</button>
+            <button className="hover:text-red-400" onClick={() => setLoginSignUpState(SignUpLoginModalPurpose.Login)}>Log In</button>
             </> : 
             <>
             <button className="mr-2 hover:text-red-400" onClick={logout}>Logout</button>
