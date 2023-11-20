df32a09f2552475626536d68471eacca5661fc39
paradox <paradoxtwitchstreamer@gmail.com>
2023-11-19 22:43:25 -0500

    Added untested helper for AuthRequired Route

--- goforum/internal/webapi/helper.go
+++ goforum/internal/webapi/helper.go
@@ -38,3 +38,30 @@ func OptionalSessionAuth(
 		return handle(c, session)
 	}
 }
+
+func RequiredAuth(
+	app *core.App,
+	handle func(*fiber.Ctx, *core.Session) error) func(*fiber.Ctx) error {
+
+	return func(c *fiber.Ctx) error {
+		session, err := CheckForSession(c, app.GetSessionManager())
+
+		if err != nil {
+			log.Printf("Error Code: %d", err.Code)
+			if err.Code == WebApiErrorTokenValidationFailed.Code {
+				return c.SendStatus(fiber.StatusUnauthorized)
+			} else {
+				if err.Code == WebApiErrorTokenInvalid.Code {
+					return c.SendStatus(fiber.StatusUnauthorized)
+				}
+				return c.SendStatus(fiber.StatusInternalServerError)
+			}
+		}
+		if session == nil {
+			return c.SendStatus(fiber.StatusUnauthorized)
+		}
+
+		return handle(c, session)
+	}
+
+}
