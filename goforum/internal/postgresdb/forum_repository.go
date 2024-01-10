b59a498e1f0c55f04f72acd17a64f87ba7428941
Neutral Paradox Dev <paradoxtwitchstreamer@gmail.com>
2024-01-09 19:16:48 -0500

    scaffold out postgresdb/forum_repository.go

--- goforum/internal/postgresdb/forum_repository.go
+++ goforum/internal/postgresdb/forum_repository.go
@@ -1,1 +1,32 @@
 package postgresdb
+
+import (
+	"github.com/neutralparadoxdev/rename-forums/goforum/internal/core"
+	"errors"
+)
+
+type ForumRepository struct {
+	db *PostgresDatabase
+}
+
+func NewForumRepository(db *PostgresDatabase) core.ForumRepository {
+	return &ForumRepository {
+		db: db,
+	}
+}
+
+func (repo *ForumRepository) GetByName(name string) (*core.Forum, error) {
+	return nil, errors.New("Not implemented")
+}
+
+func (repo *ForumRepository) Delete(forum core.Forum) error {
+	return errors.New("Not Implemented")
+}
+
+func (repo *ForumRepository) Create(title string, description string, ownerId int64, isPublic bool) error {
+	return errors.New("Not Implemented")
+}
+
+func (repo *ForumRepository) GetAll(userId *int64) ([]core.Forum, error) {
+	return nil, errors.New("Not Implemented")
+}
