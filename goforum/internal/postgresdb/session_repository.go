f9de77796837ee6dfcbbcbda349873fc282da109
Neutral Paradox Dev <paradoxtwitchstreamer@gmail.com>
2024-01-10 22:26:25 -0500

    Added session repository scaffold

--- goforum/internal/postgresdb/session_repository.go
+++ goforum/internal/postgresdb/session_repository.go
@@ -1,1 +1,29 @@
 package postgresdb
+
+import (
+	"github.com/neutralparadoxdev/rename-forums/goforum/internal/core"
+	"errors"
+)
+
+type SessionRepository struct {
+	db *PostgresDatabase
+}
+
+func NewSessionRepository(db *PostgresDatabase) core.SessionRepository {
+	return &SessionRepository {
+		db: db,
+	}
+}
+
+func (repo *SessionRepository) DoesSessionExist(session core.Session) (bool, error) {
+	return false, errors.New("Not Implemented") 
+}
+
+func (repo *SessionRepository) Delete(session core.Session) error {
+	return errors.New("Not Implemented")
+}
+
+func (repo *SessionRepository) Save(session core.Session) error {
+	return errors.New("Not Implemented")
+}
+
