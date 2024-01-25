1e333dd17f33e0199d3ed1951556f88e1e6a2db9
Neutral Paradox Dev <paradoxtwitchstreamer@gmail.com>
2024-01-24 21:54:51 -0500

    added test for comment manager create comment for comment

--- goforum/internal/core_test/comment_manager_test.go
+++ goforum/internal/core_test/comment_manager_test.go
@@ -6,7 +6,7 @@ import (
 	mockdatabase "github.com/neutralparadoxdev/rename-forums/goforum/internal/mock_database"
 )
 
-func TestCommentManager(t *testing.T) {
+func TestCommentManagerCreateCommentForPost(t *testing.T) {
 	db := mockdatabase.New()
 
 	commentManager := core.NewCommentManager(db)
@@ -50,7 +50,6 @@ func TestCommentManager(t *testing.T) {
 		return
 	}
 
-
 	id := int64(1)
 
 	text := "This is a nice post"
@@ -66,3 +65,63 @@ func TestCommentManager(t *testing.T) {
 		t.Errorf("did not expect commentId to be zero")
 	}
 }
+
+func TestCommentManagerCreateCommentForComment(t *testing.T) {
+	db := mockdatabase.New()
+
+	commentManager := core.NewCommentManager(db)
+
+	secret := "SOMETHINGSECRET"
+
+	auth := core.NewAuthenticator()
+
+	userManager := core.NewUserManager(db, auth)
+
+	sessionManager := core.NewSessionManager(
+		secret,
+		userManager,
+		auth,
+		db.GetSessionRepository(),
+	)
+
+	session, err := sessionManager.CreateSession("hello", "hello")
+
+	if err != nil {
+		t.Errorf("did not expect error from session %v", err)
+		return
+	}
+
+	empty := core.Session{}
+
+	if session == empty {
+		t.Errorf("did not expect empty session")
+		return
+	}
+
+	ok, cerr := sessionManager.VerifySession(&session)
+
+	if cerr != nil {
+		t.Errorf("sessionManager Verification failed: %v", cerr)
+		return
+	}
+
+	if !ok {
+		t.Errorf("failed to verify session")
+		return
+	}
+
+	id := int64(10)
+
+	commentId, err := commentManager.CreateCommentForComment(session, id, "Test message")
+
+	if err != nil {
+		t.Errorf("commentManager.CreateCommentForComment errored: %v", err)
+		return
+	}
+
+	if commentId == 0 {
+		t.Errorf("did not expect commentId to be zero")
+	}
+}
+
+
